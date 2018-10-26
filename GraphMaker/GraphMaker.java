3a8a2a0e83bb074e3cc2fa9407640fc5d546482e
TakMashido <takMashido2@gmail.com>
2018-10-26 15:08:29 +0200

    Updated GraphMaker tool
    Added support to draw multiple graphs on one image

--- GraphMaker/GraphMaker.java
+++ GraphMaker/GraphMaker.java
@@ -1,21 +1,21 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.awt.Color;
-import java.util.ArrayList;
-import java.util.Scanner;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 
-
 public class GraphMaker{
-	public static void main(String[]args) {
+	public static void main(String[]args) {		
 		System.setErr(System.out);
 		
-		String fileName=null;
+		ArrayList<String> fileNames=new ArrayList<String>();
 		String outFileName=null;
 		String outFormat="png";
 		int y=500;
@@ -39,7 +39,7 @@ public class GraphMaker{
 				outFormat=args[++i];
 				break;
 			case "-in":
-				fileName=args[++i];
+				fileNames.add(args[++i]);
 				break;
 			case "-i":
 				invert=true;
@@ -50,8 +50,8 @@ public class GraphMaker{
 				break;
 			case "-info":
 				System.out.println("Author:  Tak Mashido");
-				System.out.println("Version: 1.0");
-				System.out.println("Compilation date: 29.10.2018");
+				System.out.println("Version: 1.1");
+				System.out.println("Compilation date: 25.10.2018");
 				return;
 			case "-help":
 			case "-h":
@@ -63,7 +63,7 @@ public class GraphMaker{
 				System.out.println("-out: \tName of output file. Default fileName");
 				System.out.println("-format:");
 				System.out.println("-f \tFormat of saved graph image. Default .png");
-				System.out.println("-in: \tFile name. Default: first input command if not special one");
+				System.out.println("-in: \tFile name. If specyfied more then one time plots multiple graphs in one file. Default: input command before special firs special one.");
 				System.out.println("-i: \tInverts data verticaly");
 				System.out.println("-show:");
 				System.out.println("-s: \tOpen output image in default image viewer");
@@ -72,46 +72,56 @@ public class GraphMaker{
 				System.out.println("-h: \tPrints this message");
 				return;
 			default:
-				if(i==0)fileName=args[0];
+				if(fileNames.size()==i)fileNames.add(args[i]);
 				else {
 					System.out.print(args[i]+" is not correct option.");
 				}
 			}
 		}
 		
-		if(fileName==null) {
+		if(fileNames.size()==0) {
 			System.out.print("Please set input data");
 			return;
 		}
 		if(outFileName==null)
-			outFileName=fileName;
+			outFileName=fileNames.get(0);
 		
-		
-		Scanner scanner=null;
-		try {
-			scanner=new Scanner(new FileInputStream(fileName));
-		} catch (FileNotFoundException ex) {
-			System.out.println("File \""+fileName+".\" doesn't exist");
-			return;
-		}
-		
-		ArrayList<Double> dataBuffer=new ArrayList<Double>();
-		while(scanner.hasNext())
-			dataBuffer.add(Double.parseDouble(scanner.next()));
-		
+		Double[][] data=new Double[fileNames.size()][];
 		double max=-Double.MAX_VALUE;
 		double min=Double.MAX_VALUE;
 		
-		Double[] data=dataBuffer.toArray(new Double[0]);
-		for(Double value:dataBuffer) {
-			if(value>max)max=value;
-			if(value<min)min=value;
+		String file;
+		for(int i=0;i<fileNames.size();i++) {
+			file=fileNames.get(i);
+			Scanner scanner=null;
+			try {
+				scanner=new Scanner(new FileInputStream(file));
+			} catch (FileNotFoundException ex) {
+				System.out.println("File \""+file+".\" doesn't exist");
+				return;
+			}
+			
+			ArrayList<Double> dataBuffer=new ArrayList<Double>();
+			while(scanner.hasNext())
+				dataBuffer.add(Double.parseDouble(scanner.next()));
+			
+			data[i]=dataBuffer.toArray(new Double[0]);
+			for(Double value:data[i]) {
+				if(value>max)max=value;
+				if(value<min)min=value;
+			}
+			
+			scanner.close();
 		}
 		
+		int maxX=0;
+		for(Double[] elem:data)
+			maxX=elem.length>x?elem.length:x;
 		if(x==0)
-			x=data.length;
+			x=maxX;
 		
-		double yscale=(y-1)/(max-min);
+		double yScale=(y-1)/(max-min);
+		double xScale=maxX/(float)x;
 		
 		BufferedImage graph=new BufferedImage(x,y,BufferedImage.TYPE_BYTE_GRAY);
 		Graphics g=graph.getGraphics();
@@ -122,30 +132,36 @@ public class GraphMaker{
 		g.drawString(Double.toString(invert?-min:max), 2, 11);
 		g.drawString(Double.toString(invert?-max:min), 2, y-2);
 		
-		int length;
-		int end=-1;
-		int lastY=invert?0:y;
-		int newY=0;
-		for(int i=0;i<x;i++) {
-			length=(data.length-end-1)/(x-i);
-			double value=avarage(data,end+1,end+length+1)-min;
-			newY=(int)(value*yscale)-1;
-			if(!invert)newY=y-newY;
-			if((invert?lastY<newY:lastY>newY)) {
-				g.setColor(Color.GRAY);
-				g.drawLine(i, lastY, i, newY);
-				g.setColor(Color.BLACK);
+		for(int i=0;i<data.length;i++) {
+			g.setColor(getColor());
+			if(data.length>1)
+				g.drawString(fileNames.get(i), 2, 30+i*13);
+			
+			int length;
+			int end=0;
+			float endFloat=0;
+			int lastY=invert?0:y;
+			int newY=0;
+			for(int j=0;j<data[i].length;j++) {
+				endFloat+=xScale;
+				length=(int)(endFloat-end);
+				double value=avarage(data[i],end,end+length)-min;
+				newY=(int)(value*yScale)-1;
+				if(!invert)newY=y-newY;
+				if((invert?lastY<newY:lastY>newY)) {
+					g.drawLine(j, lastY, j, newY);
+				}
+				g.fillRect(j, newY, 1, 1);
+				lastY=newY;
+				end+=length;
 			}
-			g.fillRect(i, newY, 1, 1);
-			lastY=newY;
-			end+=length;
 		}
 		
 		if(show) {
-			String imageName=fileName.substring(0,fileName.indexOf('.'))+"."+outFormat;
+			String imageName=outFileName.substring(0,outFileName.indexOf('.'))+"."+outFormat;
 			try {
 				ImageIO.write(graph, outFormat, new File(imageName));
-				Runtime.getRuntime().exec("cmd /c "+fileName.substring(0,fileName.indexOf('.'))+"."+outFormat);
+				Runtime.getRuntime().exec("cmd /c "+outFileName.substring(0,outFileName.indexOf('.'))+"."+outFormat);
 			} catch(IOException ex) {
 				ex.printStackTrace();
 			}
@@ -158,5 +174,10 @@ public class GraphMaker{
 		for(int i=start;i<end;i++)
 			Return+=data[i];
 		return Return/(end-start);
+	}
+	
+	private static Random random=new Random(0);
+	private static Color getColor() {
+		return new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255));
 	}
 }
